412f9894f1e38fd1c64bc0d88d4c2049f8da858b
Ethan Blackwood <ethanbblackwood@gmail.com>
2018-10-02 12:13:00 -0500

    Update with new optimizations correctly this time

--- Source/Plugins/PhaseCalculatorOriginal/PhaseCalculator.cpp
+++ Source/Plugins/PhaseCalculatorOriginal/PhaseCalculator.cpp
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const float PhaseCalculator::PASSBAND_EPS = 0.01F;
 
 PhaseCalculator::PhaseCalculator()
-    : GenericProcessor      ("PC Original")
+    : GenericProcessor      ("Phase Calculator")
     , Thread                ("AR Modeler")
     , calcInterval          (50)
     , lowCut                (4.0)
@@ -275,10 +275,10 @@ void PhaseCalculator::process(AudioSampleBuffer& buffer)
 
             arPredict(rpBuffer, wpHilbert, predictionLength, pLocalParam, arOrder);
 
-            // Hilbert-transform dataToProcess
-            forwardPlan[activeChan]->execute();      // reads from dataToProcess, writes to fftData
+            // Hilbert-transform hilbertBuffer
+            forwardPlan[activeChan]->execute();
             hilbertManip(hilbertBuffer[activeChan]);
-            backwardPlan[activeChan]->execute();     // reads from fftData, writes to dataOut
+            backwardPlan[activeChan]->execute();
 
             // calculate phase and write out to buffer
             auto rpHilbert = hilbertBuffer[activeChan]->getComplexPointer(hilbertPastLength - nSamplesToProcess);
@@ -397,18 +397,12 @@ void PhaseCalculator::run()
     Array<double> paramsTemp;
     paramsTemp.resize(arOrder);
 
-    ARTimer timer;
-    int currInterval = calcInterval;
-    timer.startTimer(currInterval);
-
     int numActiveChans = getActiveInputs().size();
 
-    while (true)
+    uint32 startTime, endTime;
+    while (!threadShouldExit())
     {
-        if (threadShouldExit())
-        {
-            return;
-        }
+        startTime = Time::getMillisecondCounter();
 
         for (int activeChan = 0; activeChan < numActiveChans; ++activeChan)
         {
@@ -445,28 +439,11 @@ void PhaseCalculator::run()
             chanState.set(activeChan, FULL_AR);
         }
 
-        // update interval
-        if (calcInterval != currInterval)
+        endTime = Time::getMillisecondCounter();
+        int remainingInterval = calcInterval - (endTime - startTime);
+        if (remainingInterval >= 10) // avoid WaitForSingleObject
         {
-            currInterval = calcInterval;
-            timer.stopTimer();
-            timer.startTimer(currInterval);
-        }
-
-        while (!timer.check())
-        {
-            if (threadShouldExit())
-            {
-                return;
-            }
-
-            if (calcInterval != currInterval)
-            {
-                currInterval = calcInterval;
-                timer.stopTimer();
-                timer.startTimer(currInterval);
-            }
-            sleep(10);
+            sleep(remainingInterval);
         }
     }
 }
@@ -1103,25 +1080,4 @@ void PhaseCalculator::hilbertManip(FFTWArray* fftData)
 
     // set negative frequencies to 0
     FloatVectorOperations::clear(reinterpret_cast<double*>(wp + firstNegFreq), numPosNegFreqDoubles);
-}
-
-// ----------- ARTimer ---------------
-
-ARTimer::ARTimer() : Timer()
-{
-    hasRung = false;
-}
-
-ARTimer::~ARTimer() {}
-
-void ARTimer::timerCallback()
-{
-    hasRung = true;
-}
-
-bool ARTimer::check()
-{
-    bool temp = hasRung;
-    hasRung = false;
-    return temp;
 }
